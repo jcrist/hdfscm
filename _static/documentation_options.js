50ee61ca7460f069db40987069940813f184c347
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-14 21:25:42 +0000

    Update docs after building Travis build 8 of jcrist/hdfscm
    The docs were built from the branch 'master' against the commit
    37275652a1c99ff6966c70f72475c43bd0ca2d50.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/hdfscm/jobs/479599916.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+1.gf834cc8',
+    VERSION: '0.1.1+2.g3727565',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
